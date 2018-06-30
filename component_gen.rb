9a01de88fa95464bec4ec09f1742d1cf541f879a
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-06-30 22:39:04 +0530

    Rmoved store itesm from component gen

--- component_gen.rb
+++ component_gen.rb
@@ -4,17 +4,17 @@ module ComponentGen
     compInitialize(componentName)
 
     generate_react_redux_component()
-    generate_store_parts()
-    linkReducer(componentName, @reducerName, @stateName)
+    # generate_store_parts()
+    # linkReducer(componentName, @reducerName, @stateName)
     addStory(componentName)
   end
 
   def generate_react_redux_component
 
     template("component/index.js", comp_filepath(@index_file))
-    template("component/types.js", "#{comp_dir}/types.js")
-    template("component/reducers.js", comp_filepath(@reducer_file) )
-    template("component/actions.js", comp_filepath(@actions_file))
+    # template("component/types.js", "#{comp_dir}/types.js")
+    # template("component/reducers.js", comp_filepath(@reducer_file) )
+    # template("component/actions.js", comp_filepath(@actions_file))
     create_file("#{comp_dir}/#{@name_l}.css")
   end
 
